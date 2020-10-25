0a37e82b032841e903453bb787aeb43ca051586c
justBoringUsername <63564366+justBoringUsername@users.noreply.github.com>
2020-10-25 19:31:17 +0300

    Add files via upload

---
+++ TracerLib/MethodTracer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TracerLib
+{
+    class MethodTracer
+    {
+        public MethodTracer()
+        {
+            Stopwatch = new Stopwatch();
+            ChildMethods = new List<MethodInfo>();
+        }
+
+        private readonly Stopwatch Stopwatch;
+
+        private readonly List<MethodInfo> ChildMethods;
+
+        public void AddChildMethod(MethodInfo childMethod)
+        {
+            ChildMethods.Add(childMethod);
+        }
+
+        public List<MethodInfo> GetChildMethods()
+        {
+            return ChildMethods;
+        }
+
+        public double GetExecutionTime()
+        {
+            return Stopwatch.ElapsedMilliseconds;
+        }
+
+        public void StartTrace()
+        {
+            Stopwatch.Start();
+        }
+
+        public void StopTrace()
+        {
+            Stopwatch.Stop();
+        }
+    }
+}
---
+++ TracerLib/Tracer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TracerLib
+{
+    public class Tracer : ITracer
+    {
+        public Tracer()
+        {
+            ThreadTracers = new ConcurrentDictionary<int, ThreadTracer>();
+        }
+
+        private readonly ConcurrentDictionary<int, ThreadTracer> ThreadTracers;
+
+        public TraceResult GetTraceResult()
+        {
+            var threadsInfo = new List<ThreadInfo>();
+            foreach (var thread in ThreadTracers)
+            {
+                threadsInfo.Add(new ThreadInfo(thread.Key, thread.Value.GetThreadMethodList()));
+            }
+            return new TraceResult(threadsInfo);
+        }
+
+        private ThreadTracer GetCurrentThreadTracer()
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;         
+            ThreadTracer threadTracer;
+            ThreadTracers.TryGetValue(threadId, out threadTracer);
+            return threadTracer;
+        }
+
+        public void StartTrace()
+        {
+            ThreadTracer threadTracer = GetCurrentThreadTracer();
+            if (threadTracer == null)
+            {
+                int currentThreadId = Thread.CurrentThread.ManagedThreadId;
+                threadTracer = new ThreadTracer();       
+                ThreadTracers.TryAdd(currentThreadId, threadTracer);
+                
+            }
+            threadTracer.StartTrace();
+        }
+
+        public void StopTrace()
+        {
+            ThreadTracer threadTracer = GetCurrentThreadTracer();
+            threadTracer.StopTrace();
+        }
+    }
+}
